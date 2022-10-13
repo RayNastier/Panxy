c440aff90f8a313cc87e296de54dd263405c957a
RMehdid <rmehdid@harbineldjazair.com>
2022-10-13 15:50:28 +0100

    added xcode project

---
+++ Panxy/PanxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PanxyApp.swift
+//  Panxy
+//
+//  Created by Samy Mehdid on 13/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct PanxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
